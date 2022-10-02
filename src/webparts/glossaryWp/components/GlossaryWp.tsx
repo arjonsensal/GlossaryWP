0aba8e4476547c380bbf1342f9e41bda8f4cda24
arjonsensal <arjonsensal@gmail.com>
2022-10-03 01:11:23 +0800

    update 10222

--- src/webparts/glossaryWp/components/GlossaryWp.tsx
+++ src/webparts/glossaryWp/components/GlossaryWp.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import styles from './GlossaryWp.module.scss';
 import { IGlossaryWpProps } from './IGlossaryWpProps';
-import { escape } from '@microsoft/sp-lodash-subset';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Pagination from 'react-bootstrap/Pagination';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
 import { SPHttpClient } from '@microsoft/sp-http';
+import Form from 'react-bootstrap/Form';
+import ReactTooltip from 'react-tooltip';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -16,6 +14,7 @@ export interface IGlossaryViewState {
   filtered: any[];
   def: any[];
   currentPage: string;
+  contentHeaders: any[];
 }
 
 export default class GlossaryWp extends React.Component<IGlossaryWpProps, IGlossaryViewState, {}> {
@@ -25,7 +24,8 @@ export default class GlossaryWp extends React.Component<IGlossaryWpProps, IGloss
       items: [],
       def: [],
       filtered: [],
-      currentPage: "A"
+      currentPage: "A",
+      contentHeaders: []
     };
   }
 
@@ -47,11 +47,11 @@ export default class GlossaryWp extends React.Component<IGlossaryWpProps, IGloss
     }
     return a;
   }
-  private items = [{id:1, title: "Arjon", content: "Hello"},{id:2, title: "Barjon", content: "Hello there"}];
+
   private handlePagination(e) {
-    // this._loadListItem();
+    var headers = this.state.contentHeaders;
     let filtered = this.state.items.filter(function (items) {
-      return items.title.charAt(0) === e.target.text;
+      return items[headers[0]].charAt(0) === e.target.text;
     })
     this.setState({
       filtered: filtered ? filtered : [],
@@ -59,79 +59,145 @@ export default class GlossaryWp extends React.Component<IGlossaryWpProps, IGloss
     });
   }
 
-  public _loadListItem() { 
-    const restApi = `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('${this.props.listName}')/items`;
-    const restApi1 = `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('${this.props.definitions}')/items`;
-    this.props.context.spHttpClient.get(restApi, SPHttpClient.configurations.v1)
-      .then(resp => { return resp.json(); })
-      .then(items => {
-        this.props.context.spHttpClient.get(restApi1, SPHttpClient.configurations.v1)
-        .then(resp1 => { return resp1.json(); })
-        .then(items1 => {
-          let tempDef = [];
-          items1.value.forEach((item) => {
-            tempDef.push({title: item.Title, definition: item.DefinitionDescription})
+  public _loadListItem () { 
+    const glossaryItemsApi = `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('${this.props.listName}')/items`;
+    const definitApi = `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('${this.props.definitions}')/items`;
+    const glossaryFieldsApi = `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('${this.props.listName}')/fields?$filter=ReadOnlyField eq false and Hidden eq false`;
+
+    const getResponse = async (api) => {
+      return this.props.context.spHttpClient.get(api, SPHttpClient.configurations.v1)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+      });
+    };
+
+    // get glossary fields
+    const glossaryFields = getResponse(glossaryFieldsApi);
+    // get glossary items
+    const glossaryItems = getResponse(glossaryItemsApi);
+    glossaryFields.then(resp => {
+      var tmp = [];
+      resp.value.forEach(val => {
+        if (val.Title !== "Attachments" && val.Title !== "Content Type") {
+          tmp.push(val.Title);
+        }
+      });
+      this.setState({
+        contentHeaders: tmp.length > 0 ? tmp : []
+      });
+
+      glossaryItems.then(resp => {
+        var tmp1 = [];
+        resp.value.forEach((item, index) => {
+          var obj = {id: index + 1};
+          this.state.contentHeaders.forEach(ch => {
+            obj[ch] = item[ch] ? item[ch] : "";
           });
-          this.setState({
-            def: tempDef ? tempDef : []
-          });
-          console.log(this.state.def)
-        });
-        let tempIt = [];
-        items.value.forEach((item, index) => {
-          tempIt.push({id: index, title: item.Title, content: item.GlossaryDesc})
+          tmp1.push(obj)
         });
         this.setState({
-          items: tempIt ? tempIt : [],
-          filtered: tempIt ? tempIt.filter(function (items) { return items.title.charAt(0) === 'A';}) : []
+          items: tmp1.length > 0 ? tmp1 : [],
+          filtered: tmp1.length > 0 ? tmp1.filter(function (items) { return items[tmp[0]].charAt(0) === 'A';}) : []
         });
       });
+    });
+
+    // get definitions
+    if (!this.props.definitions) {
+      return;
+    }
+    const definitions = getResponse(definitApi);
+    definitions.then(resp => {
+      var tmp = [];
+      resp.value.forEach((item, index) => {
+        tmp.push({title: item.Title, definition: item.DefinitionDescription})
+      });
+      this.setState({
+        def: tmp.length > 0 ? tmp : []
+      });
+    });
+  }
+
+  public handleSearchBar = (e) => {
+    var keyword = e.target.value;
+    var keywordL = keyword.length;
+    var headers = this.state.contentHeaders;
+    if (keyword != "" && keywordL >= 1) {
+      let filtered = this.state.items.filter(function (items) {
+        return items[headers[0]].toLowerCase().includes(keyword.toLowerCase());
+      });
+      filtered.sort(function(a, b) {
+        return a[headers[0]].localeCompare(b[headers[0]])
+      });
+      this.setState({
+        filtered: filtered.length !== 0 ? filtered : [],
+        currentPage: ''
+      });
+    }
   }
 
   private constr(content, defs) {
-    console.log(defs)
-    var words = content.split(" ");
-    var finalCont = "";
-    console.log(words, defs)
-    words.forEach((word, i) => {
-      defs.forEach(def => {
-          if (def.title === word) {
-            console.log(words[i])
-            words[i] = <OverlayTrigger overlay={<Tooltip>{def.definition}</Tooltip>}>
-                  <span style={{textDecorationLine: 'underline', textDecorationStyle: 'dashed'}}>
-                    {def.title}
-                  </span>
-                </OverlayTrigger>;
+    // definitions
+    const addDefin = (cont) => {
+      var words = cont.split(" ");
+      words.forEach((word, i) => {
+        defs.forEach(def => {
+            if (word.includes(def.title)) {
+              words[i] = `<span data-tip=${def.definition} style="text-decoration: underline dotted">${word}</span>`;
+            }
+        })
+        if (typeof(words[i]) === "string") {
+          words[i] = words[i] + " ";
+          if (words[i - 1]) {
+            if (typeof(words[i - 1]) !== "string") {
+              words[i] = " " + words[i];
+            }
           }
-      })
-      if (typeof(words[i]) === "string") {
-        words[i] = words[i] + " ";
-        if (words[i - 1]) {
-          if (typeof(words[i - 1]) !== "string") {
-            words[i] = " " + words[i];
-          }
+        } else {
+          words[i] = words[i]
         }
-      } else {
-        words[i] = words[i]
-      }
-    })
-    return words
+      })
+      return words.join(" ");
+    };
+
+    var headrs = this.state.contentHeaders.filter(hd => hd !== this.state.contentHeaders[0]);
+    var fullContent = "";
+    headrs.forEach(header => {
+      var titleH = `<h6>${header}</h6>`;
+      var body = `<p>${addDefin(content[header])}</p>`;
+      fullContent += (titleH + body);
+    });
+
+    return <div> <div dangerouslySetInnerHTML={{ __html: fullContent }}></div>
+        <ReactTooltip effect="solid" />
+      </div>
   }
 
   public render(): React.ReactElement<IGlossaryWpProps> {
     return (
       <div>
-        <h1>Glossary</h1>
-        <Pagination style={{justifyContent: 'left'}} size="sm">{this.genCharArray("A", "Z")}</Pagination>
-        {this.state.filtered.map((item) => {
-        return <Accordion key={(item.id === 0 ? 1 : item.id)} defaultActiveKey={this.state.items.length + 1}>
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey={(item.id === 0 ? 1 : item.id)}>
-              {item.title}
+        <h2 className='text-center'>Glossary</h2>
+        <Form className="d-flex" style={{padding: '10px'}}>
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            onChange={this.handleSearchBar}
+          />
+        </Form>
+        <Pagination style={{justifyContent: 'center'}} size="sm">{this.genCharArray("A", "Z")}</Pagination>
+        {(this.state.filtered.length != 0) && this.state.filtered.map((item) => {
+        return <Accordion key={item.id}>
+          <Card style={{ width: '45rem' }}>
+            <Accordion.Toggle as={Card.Header} eventKey={item.id}>
+              {item[this.state.contentHeaders[0]]}
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey={(item.id === 0 ? 1 : item.id)}>
+            <Accordion.Collapse eventKey={item.id}>
               <Card.Body>
-              {this.constr(item.content, this.state.def)}</Card.Body>
+              {this.constr(item, this.state.def)}</Card.Body>
             </Accordion.Collapse>
           </Card>
         </Accordion>})}
